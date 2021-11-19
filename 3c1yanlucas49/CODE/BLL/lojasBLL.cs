49dec768e93b237c4cab08b980aea3be779db642
YanLucas21901830 <94720237+YanLucas21901830@users.noreply.github.com>
2021-11-19 19:14:04 -0300

    Add files via upload

---
+++ 3c1yanlucas49/CODE/BLL/lojasBLL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _3c1yanlucas49.CODE.DTO; 
+using _3c1yanlucas49.CODE.DAL;
+using System.Data;
+
+namespace _3c1yanlucas49.CODE.BLL
+{
+    class lojasBLL
+    {
+       
+        bd bd = new bd();
+        string tabela = "funcionarios";
+
+
+        
+        public void Inserir(LojasDTO lojasDTO)
+        {
+            
+            string inserir = $"insert into {tabela} values('{ lojasDTO.Id}','{ lojasDTO.Profissao}','{ lojasDTO.Salario}')";
+            bd.ExecutarComando(inserir);
+        }
+
+
+        public void Excluir(LojasDTO lojasDTO)
+        {
+            string excluir = $"delete from {tabela} where id = '{lojasDTO.Id}';";
+            bd.ExecutarComando(excluir);
+        }
+
+        public DataTable Listar()
+        {
+            string sql = $"select * from {tabela}; ";
+            return bd.ExecutarConsulta(sql);
+        }
+        public void Editar(LojasDTO lojasDTO)
+        {
+            string alterar = $"update {tabela} set profissao = '{lojasDTO.Profissao}', salario = '{lojasDTO.Salario}' where id = '{lojasDTO.Id}';";
+            bd.ExecutarComando(alterar);
+        }
+    }
+}
---
+++ 3c1yanlucas49/CODE/DTO/LojasDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3c1yanlucas49.CODE.DTO
+{
+    class LojasDTO
+    {
+        private int _id;
+        private string _profissao;
+        private int _salario;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Profissao { get => _profissao; set => _profissao = value; }
+        public int Salario { get => _salario; set => _salario = value; }
+    }
+}
